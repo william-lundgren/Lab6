36eed09db40739b144c1e1b42214d23a5207fdce
william-lundgren <superbanan@live.se>
2024-05-21 11:36:01 +0200

    a little more

--- solution_file.py
+++ solution_file.py
@@ -16,10 +16,7 @@ class Route:
 
 def parse_input():
     # Input() takes line by line
-
     N, M, C, P = list(map(int, input().split()))
-
-    # For every letter we get a list of the cost to every other letter, loop through all of them
 
     # Take all the inputs from ever line with u, v, and c and make into ints split them make into route and add
     # route obj to routes
@@ -27,10 +24,19 @@ def parse_input():
 
     # Edges to be removed
     to_remove = [int(input()) for _ in range(P)]
+    return routes, to_remove
+
+def algorithm():
+    # GOLDBERG TIME
 
 
 def main():
-    parse_input()
+    routes, removed = parse_input()
+
+
+
+
+    algorithm()
 
 
 if __name__ == "__main__":
