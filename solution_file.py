1f920a77e684db0b53b555d6c8d983621f02f65c
william-lundgren <superbanan@live.se>
2024-05-21 10:53:36 +0200

    fan vad vi parsat

--- solution_file.py
+++ solution_file.py
@@ -1,29 +1,36 @@
 import sys
 
 
+class Route:
+    def __init__(self, n1, n2, c):
+        self.node_1 = n1
+        self.node_2 = n2
+        self.flow = c
+
+    def __str__(self):
+        return f"({self.node_1}, {self.node_2}, Flow:{self.flow})"
+
+    def __repr__(self):
+        return self.__str__()
+
+
 def parse_input():
     # Input() takes line by line
 
-    N, C, M, P = input().split()
-
+    N, M, C, P = list(map(int, input().split()))
 
     # For every letter we get a list of the cost to every other letter, loop through all of them
-    # Convert the costs to int and add to list
-    costs = [int(val) for val in input().split()]
 
-    # Add the cost between current character and every other character
-    for i, cost in enumerate(costs):
-        gain_map[c][characters[i]] = cost
-    # Next we get the query amount
-    Q = int(input())
-    queries = [input().split() for _ in range(Q)]
+    # Take all the inputs from ever line with u, v, and c and make into ints split them make into route and add
+    # route obj to routes
+    routes = [Route(*input().split()) for _ in range(M)]
 
-    # Return 2D array and comparisons we want to do
-    return gain_map, queries
+    # Edges to be removed
+    to_remove = [int(input()) for _ in range(P)]
 
 
 def main():
-    pass
+    parse_input()
 
 
 if __name__ == "__main__":
